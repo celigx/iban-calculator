b5c3ec2d411a921818fed960a8227cf8f0953d62
Celig <github@cela.anonaddy.com>
2022-07-06 20:37:59 +0200

    Add list of bank identifiers

---
+++ src/helper/BankIdentifierList.tsx
@@ -0,0 +1,41 @@
+type AutocompleteOption = {
+  label: string,
+  bankId: number
+};
+
+export const bankIdentifierList: AutocompleteOption[] = [
+  { label: "4115008 - Banco Popolare Croatia d.d.", bankId: 411500 },
+  { label: "4133006 - Banka Kovanica d.d.", bankId: 413300 },
+  { label: "4109006 - Banka Splitsko-Dalmatinska d.d.", bankId: 4109006 },
+  { label:"2488001 - BKS Bank d.d.", bankId: 2488001 },
+  { label:"2485003 - Croatia Banka d.d.", bankId: 2485003 },
+  { label:"2402006 - Erste & Steiermärkische Bank d.d.", bankId: 2402006 },
+  { label:"2493003 - Hrvatska banka za obnovu i razvitak", bankId: 2493003 },
+  { label:"1001005 - Hrvatska Narodna Banka", bankId: 1001005 },
+  { label:"2390001 - Hrvatska Poštanska Banka d.d.", bankId: 2390001 },
+  { label:"2500009 - Hypo Alpe-Adria-Bank d.d.", bankId: 2500009 },
+
+  { label:"2492008 - Imex Banka d.d.", bankId: 2492008 },
+  { label:"2380006 - Istarska Kreditna Banka Umag d.d.", bankId: 2380006 },
+  { label:"2411006 - Jadranska Banka d.d.", bankId: 2411006 },
+  { label:"2400008 - Karlovačka Banka d.d.", bankId: 2400008 },
+  { label:"4124003 - Kentbank d.d.", bankId: 4124003 },
+  { label:"2481000 - Kreditna Banka Zagreb d.d.", bankId: 2481000 },
+  { label:"2495009 - Nava Banka d.d.", bankId: 2495009 },
+  { label:"2407000 - OTP Banka Hrvatska d.d.", bankId: 2407000 },
+  { label:"2408002 - Partner Banka d.d.", bankId: 2408002 },
+  { label:"2386002 - Podravska Banka d.d.", bankId: 2386002 },
+  { label:"4132003 - Primorska Banka d.d.", bankId: 4132003 },
+
+  { label:"2340009 - Privredna Banka Zagreb d.d.", bankId: 2340009 },
+  { label:"2484008 - Raiffeisenbank Austria d.d.", bankId: 2484008 },
+  { label:"2403009 - Samoborska Banka d.d.", bankId: 2403009 },
+  { label:"2503007 - Sberbank d.d.", bankId: 2503007 },
+  { label:"2412009 - Slatinska Banka d.d.", bankId: 2412009 },
+  { label:"2330003 - Societe Generale- Splitska Banka d.d.", bankId: 2330003 },
+  { label:"2483005 - Štedbanka d.d.", bankId: 2483005 },
+  { label:"6717002 - Tesla Štedna Banka d.d.", bankId: 6717002 },
+  { label:"2489004 - Vaba d.d. Banka", bankId: 2489004 },
+  { label:"2381009 - Veneto Banka d.d", bankId: 2381009 },
+  { label:"2360000 - Zagrebačka Banka d.d.", bankId: 2360000 }
+];
