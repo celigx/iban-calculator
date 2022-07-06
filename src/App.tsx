a8491d82357b97562517e8c86fbcafbdc1e04d03
Celig <github@cela.anonaddy.com>
2022-07-06 20:38:36 +0200

    Add autocomplete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import './App.css';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import './App.sass';
+import { bankIdentifierList } from './helper/BankIdentifierList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className="titleContainer">
+        <h1 className="title">IBAN kalkulator</h1>
+      </div>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={bankIdentifierList}
+        renderInput={(params) => <TextField {...params} label="Bank Identifier" />}
+      />
     </div>
   );
 }
