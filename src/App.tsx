8cc0e88a97db2c3311f8b9d1bd541d2ae7bacf49
Celig <github@cela.anonaddy.com>
2022-07-06 23:04:11 +0200

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import './App.sass';
 import { bankIdentifierList } from './helper/BankIdentifierList';
 
 function App() {
+  const [bankIdentifier, setBankIdentifier] = useState<number>()
+  const [accountNumber, setAccountNumber] = useState<string>()
+
+  const handleBankIdentifier = (e: any, newValue: any) => {
+    setBankIdentifier(newValue.bankId)
+  }
+
+  const handleAccountNumber = (e: any) => {
+    setAccountNumber(e.target.value)
+  }  
+
   return (
     <div className="app">
 
@@ -20,9 +31,16 @@ function App() {
           id="combo-box-demo"
           options={bankIdentifierList}
           renderInput={(params) => <TextField {...params} label="Odaberite vodeći broj banke" />}
+          onChange={handleBankIdentifier}
           />
         {/* Account number */}
-        <TextField className='textField' id="outlined-basic" label="Unesite broj računa" variant="outlined" />
+        <TextField 
+          className='textField' 
+          id="outlined-basic" 
+          label="Unesite broj računa" 
+          variant="outlined"
+          onChange={handleAccountNumber}
+        />
       </div>
     </div>
   );
