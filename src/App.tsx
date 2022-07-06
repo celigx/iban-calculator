d76f71733283cb4535618c62d79f4f9e29029d4e
Celig <github@cela.anonaddy.com>
2022-07-06 21:05:34 +0200

    Add bank account input

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,23 @@ import { bankIdentifierList } from './helper/BankIdentifierList';
 function App() {
   return (
     <div className="app">
+
       <div className="titleContainer">
         <h1 className="title">IBAN kalkulator</h1>
       </div>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={bankIdentifierList}
-        renderInput={(params) => <TextField {...params} label="Bank Identifier" />}
-      />
+
+      <div className="inputContainer">
+        {/* Bank identifier */}
+        <Autocomplete
+          className='autocomplete'
+          disablePortal
+          id="combo-box-demo"
+          options={bankIdentifierList}
+          renderInput={(params) => <TextField {...params} label="Odaberite vodeći broj banke" />}
+          />
+        {/* Account number */}
+        <TextField className='textField' id="outlined-basic" label="Unesite broj računa" variant="outlined" />
+      </div>
     </div>
   );
 }
